4430780b79504c82a91a6277c2eeebf8d35cbaeb
Adrián <adrianalebrizuela@gmail.com>
2019-09-25 09:55:47 -0300

    Add project files.

---
+++ CentralitaPoliformismo.Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaPoliformismo.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
